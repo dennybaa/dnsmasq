d5fb3da48665fed8846b30024bd1f0fe5e7deda8
Sergey Melnik <admin.sa@gmail.com>
2014-04-30 13:26:36 +0400

    Stop if no config

--- recipes/dns.rb
+++ recipes/dns.rb
@@ -7,7 +7,7 @@ unless(node[:dnsmasq][:enable_dhcp])
 end
 
 # Check resolvconf usage
-unless %x(which resolvconf).empty? || ::File.exist?('/etc/dnsmasq.d/dns.conf')
+unless ::File.exist?('/etc/dnsmasq.d/dns.conf')
   service 'dnsmasq' do
     Chef::Log.error("resolvconf package is used but /etc/dnsmasq.d/dns.conf not found, resolving is not possible.")
     action [:stop]
